0f381019a0b4f87611c69c4eb895237aa179b3f3
BLITzZ0 <ababhishek3005@gmail.com>
2025-08-08 18:08:11 +0530

    Few Updates in bank account routes

--- backend/routes/Account.js
+++ backend/routes/Account.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const isAuthenticated = require("../Middleware/AuthMiddleware.js");
-import { Account } from '../db.js';
+const { Account } = require('../db.js');
 
-router.get("/balance", isAuthenticated,(req,res)=>{
-    
+router.post("/balance", isAuthenticated,async(req,res)=>{
+    try{
+        const userId = req.UserId
+        const User = await Account.findOne({userId:userId})
+        if(!User){
+            return res.status(401).json({message:"User Not found"})
+    }
+    return res.status(200).json({Balance : (User.balance / 100)})
+    }catch(err){
+        return res.status(500).json({Error:err.message})
+    }
 })
 
 
