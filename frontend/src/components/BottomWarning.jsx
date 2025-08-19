733bfebd837f72743c80ba0ccee6d18b31539efe
BLITzZ0 <ababhishek3005@gmail.com>
2025-08-19 23:06:29 +0530

    Fixed Dashboard

--- frontend/src/components/BottomWarning.jsx
+++ frontend/src/components/BottomWarning.jsx
@@ -1,12 +1,12 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export function BottomWarning({label,buttontext,to}){
-    return <div className="py-2 text-sm flex justify-center">
-        <div>
-            {label}
-        </div>
-        <Link rel="stylesheet" to={to} >
-            {buttontext}
-        </Link>
+export function BottomWarning({ label, buttontext, to }) {
+  return (
+    <div className="py-2 text-sm flex justify-center gap-1">
+      <div>{label}</div>
+      <Link to={to} className="text-blue-500 hover:underline">
+        {buttontext}
+      </Link>
     </div>
+  );
 }
--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,5 +1,10 @@
-export function Button({label, onClick}){
-    return <button onClick={onClick} className="flex items-center justify-center border border-gray-700 bg-gray-700 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-gray-800 focus:outline-none focus:shadow-outline">
-		{label}
-	</button>
+export function Button({ label, onClick }) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex items-center justify-center border border-blue-600 bg-blue-600 text-white rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+    >
+      {label}
+    </button>
+  );
 }
